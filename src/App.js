aaf6a6508f830cb162459ffbee86875b4fb9b72f
Gilberto Asuaje <asuajegilberto@gmail.com>
2020-03-30 20:31:38 -0500

    upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react'
+import './App.css'
+import { Alert } from './components/alert'
+import NavBar from './components/navbar'
+import Search from './components/search'
+import Users from './components/Users'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { About } from './components/about'
+import User from './components/user'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import GithubState from './context/github/GithubState'
+import AlertState from './context/alert/AlertState'
+import NotFound from './components/notfound'
+import Home from './home'
+
+const App = ()=>{
+     
+      return (
+      <div>
+      <GithubState>
+        <AlertState>
+          <Router>
+            <NavBar title="GitHub App" icon ="fab fa-github" />
+            <Alert />
+            <Switch>
+              <Route exact path="/" component ={Home} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/user/:login" render={props=>(
+                <User
+                  {...props} />
+              )} />
+              <Route component={NotFound} />
+              </Switch>
+            </Router>
+          </AlertState>
+        </GithubState>
+        
+      </div>
+    );
+   
 }
 
 export default App;
