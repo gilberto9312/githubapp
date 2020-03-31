c16d0bb88e7050e15384b39f76be8ad4c6c24562
Gilberto Asuaje <asuajegilberto@gmail.com>
2020-03-30 20:41:33 -0500

    about

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -6,7 +6,7 @@ export const About = () => {
             <h1>About this page</h1>
             <p>this page is a integration with github api</p>
             <p>to extend my portfolio</p>
-            <p>see code click here</p>
+            <p><a href="https://github.com/gilberto9312/githubapp" target="blank">see code click here</a></p>
         </div>
     )
 }
